ebdb13531cd864a2c39f7bba9284053f8eb8ac11
Neethu Vasundharan Sheeja <34731410+Nethzz@users.noreply.github.com>
2026-01-08 17:38:50 +0100

    Update footer.jsx

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -30,7 +30,7 @@ const Footer = () =>
 
 				<div className="footer-credits">
 					<div className="footer-credits-text">
-						© 2025 | All Rights Reserved.
+						© 2026 | All Rights Reserved.
 					</div>
 				</div>
 			</div>
