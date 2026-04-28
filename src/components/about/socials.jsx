767f6b88867ed9e84af46acc3904dd9dee1c3752
Neethu Vasundharan Sheeja <34731410+Nethzz@users.noreply.github.com>
2026-04-28 12:55:55 +0200

    social

--- src/components/about/socials.jsx
+++ src/components/about/socials.jsx
@@ -3,10 +3,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import
 {
-	faTwitter,
 	faGithub,
 	faLinkedin,
-	faInstagram,
 	faMedium
 } from "@fortawesome/free-brands-svg-icons";
 
